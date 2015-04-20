0de3bc025c9bbba421725e9e81d93c7241d15909
Matthew Wedgwood <mw@rmn.com>
2015-04-20 09:58:58 -0500

    Initial stab at compose support
      * Emulate /bin/echo
      * Remove some cruft from .gitignore

--- data-container/setup.go
+++ data-container/setup.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	var uid uint64 = 0
 	var gid uint64 = 0
+
+	if path.Base(os.Args[0]) == "echo" {
+		fmt.Print(strings.Join(os.Args[1:], " "))
+		fmt.Print("\n")
+		os.Exit(0)
+	}
 
 	if len(os.Args) > 1 {
 		uid, _ = strconv.ParseUint(os.Args[1], 10, 0)
